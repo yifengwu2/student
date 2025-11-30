1965c6b42c5b8076dfb7b4b6603b6b5bcfa2e871
yifengwu2 <2454495978@qq.com>
2025-11-30 23:48:50 +0800

    将学生进阶成责任链

--- Student.java
+++ Student.java
@@ -2,32 +2,97 @@ package com.student;
 
 public class Student {
     //身份证
-    private int idCard;
+    private final int idCard;
     //学号
-    private int number;
+    private final int number;
     //专业
-    private String sub;
+    private final String sub;
     //姓名
-    private String name;
+    private final String name;
     //性别
-    private String sex;
+    private final String sex;
 
-    private int age;
+    private final int age;
 
-    private Score socre;
+    private  final Score socre;
 
-    public Student(int idCard, int age, String sex, String name, String sub, int number) {
-        this.idCard = idCard;
-        this.age = age;
-        this.sex = sex;
-        this.name = name;
-        this.sub = sub;
-        this.number = number;
+    private Builder builder;
+
+
+    private Student(Builder builder) {
+        this.idCard = builder.idCard;
+        this.sub = builder.sub;
+        this.age = builder.age;
+        this.number = builder.number;
+        this.name = builder.name;
+        this.sex = builder.sex;
+        this.socre=builder.socre;
     }
 
-    public void setSocre(Score socre) {
-        this.socre = socre;
+
+    static class Builder {
+        private int idCard;
+        //学号
+        private int number;
+        //专业
+        private String sub;
+        //姓名
+        private String name;
+        //性别
+        private String sex;
+
+        private int age;
+
+        private Score socre;
+
+
+        public Builder setIdCard(int idCard) {
+            this.idCard = idCard;
+            return this;
+        }
+
+        public Builder setNumber(int number) {
+            this.number = number;
+            return this;
+
+        }
+
+        public Builder setSub(String sub) {
+            this.sub = sub;
+            return this;
+        }
+
+        public Builder setName(String name) {
+            this.name = name;
+            return this;
+        }
+
+        public Builder setSex(String sex) {
+            this.sex = sex;
+            return this;
+
+        }
+
+        public Builder setAge(int age) {
+            this.age = age;
+            return this;
+        }
+
+        public Builder setSocre(Score socre) {
+            this.socre = socre;
+            return this;
+        }
+
+        public Student build() {
+            return new Student(this);
+        }
+
+
     }
+
+//    private void setSocre(Score socre) {
+//        this.socre = socre;
+//    }
 
     @Override
     public String toString() {
@@ -42,7 +107,7 @@ public class Student {
                 '}';
     }
 
-     class Score {
+    static class Score {
         int EnglishScore;
         int JavaSocrere;
         int MathScore;
@@ -64,10 +129,8 @@ public class Student {
     }
 
     public static void main(String[] args) {
-//       Score score = new Score(13, 42, 32);
-        Student student = new Student(12345, 23, "男", "小明", "数据", 4312);
-        Score score = student.new Score(11, 31, 42);
-        student.setSocre(score);
+        Score score = new Score(13, 42, 32);
+        Student student = new Builder().setSocre(score).setAge(11).setName("xx").setIdCard(123).setSub("fas").setSex("男").setNumber(1324).build();
         System.out.println(student);
 
     }
