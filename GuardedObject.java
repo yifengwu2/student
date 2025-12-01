37ae320b968a61173cce1b9c2adee6ad6577ffce
yifengwu2 <2454495978@qq.com>
2025-12-01 23:41:41 +0800

    Guarded Suspension 模式（保护性暂停）

---
+++ GuardedObject.java
@@ -0,0 +1,105 @@
+package com.stupra;
+
+import lombok.extern.slf4j.Slf4j;
+
+import java.util.Hashtable;
+import java.util.Map;
+import java.util.Set;
+
+@Slf4j(topic = "GuardedObject")
+public class GuardedObject {
+    private int id;
+    private Object response;
+
+    public GuardedObject(int id) {
+        this.id = id;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    //等待结果
+    public Object get(long timeout) {
+        synchronized (this) {
+            log.debug("开始获取信息");
+            long begin = System.currentTimeMillis();
+            long parseTime = 0;
+            while (response == null) {
+                long waitTime = timeout - parseTime;
+                if (waitTime <= 0) {
+                    break;
+                }
+                log.debug("开始等待");
+                try {
+                    this.wait(waitTime);
+                } catch (InterruptedException e) {
+                    System.out.println(e.getMessage());
+                }
+                parseTime = System.currentTimeMillis() - begin;
+            }
+        }
+        return response;
+    }
+
+    //传输结果
+    public void complete(Object object) {
+        synchronized (this) {
+            response = object;
+            this.notifyAll();
+        }
+    }
+}
+
+//中间层面，存guarded
+class Mailboxes {
+    private static int id;
+    private static Map<Integer, GuardedObject> boxes = new Hashtable<>();
+
+    public static GuardedObject createGuardedObject() {
+        GuardedObject obj = new GuardedObject(getId());
+        boxes.put(obj.getId(), obj);
+        return obj;
+    }
+
+    public synchronized static int getId() {
+        return ++id;
+    }
+
+    public static GuardedObject getGuardedObject(int id) {
+        return boxes.get(id);
+    }
+
+
+    public static Set<Integer> getIds() {
+        return boxes.keySet();
+    }
+}
+
+@Slf4j
+class People extends Thread {
+    @Override
+    public void run() {
+        GuardedObject object = Mailboxes.createGuardedObject();
+        log.debug("收信人id{}", object.getId());
+        Object o = object.get(5000);
+        log.debug("收信人id{},内容{}", object.getId(), o);
+    }
+}
+
+class PostMan extends Thread {
+    private int id;
+    private String mail;
+
+    public PostMan(int id, String mail) {
+        this.id = id;
+        this.mail = mail;
+    }
+
+
+    @Override
+    public void run() {
+        GuardedObject guardedObject = Mailboxes.getGuardedObject(id);
+        guardedObject.complete(mail + id);
+    }
+}
