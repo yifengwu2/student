531f3a08d5e971f252d5dd57a4a0d8d4a3da29da
yifengwu2 <2454495978@qq.com>
2025-12-11 20:38:07 +0800

    出栈(带超时)保护性暂停带超时

--- Example09.java
+++ Example09.java
@@ -5,15 +5,18 @@ import lombok.extern.slf4j.Slf4j;
 import java.util.Deque;
 import java.util.LinkedList;
 
+/**
+ * 生产者-消费者-仓储模型（LIFO 栈语义）
+ */
 public class Example09 {
     public static void main(String[] args) {
         //三个生产者
         for (int i = 0; i < 3; i++) {
-            new Producer("生产" + i).start();
+            new Producer("生产者" + i).start();
         }
         //五个消费者
         for (int i = 0; i < 3; i++) {
-            new Consumer("消费" + i).start();
+            new Consumer("消费者" + i).start();
         }
     }
 }
@@ -113,6 +116,29 @@ class Storehouse {
         }
     }
 
+    //出栈(带超时)保护性暂停带超时
+    public String pop(long timeout) {
+        synchronized (stack) {
+            long start = System.currentTimeMillis();
+            while (stack.isEmpty()) {
+                long end = System.currentTimeMillis();
+                long parseTime = end - start;
+                long waitTime = timeout - parseTime;
+
+                if (waitTime <= 0) {
+                    return null;
+                }
+                try {
+                    stack.wait(waitTime);
+                } catch (InterruptedException e) {
+                    log.error("{}被中断", Thread.currentThread().getName(), e);
+                    throw new RuntimeException("Interrupted", e);
+                }
+            }
+            return stack.pollLast();
+        }
+    }
+
     //获取容量
     public int getSize() {
         synchronized (stack) {
@@ -135,7 +161,7 @@ class Consumer extends Thread {
         Storehouse storehouse = Storehouse.getInstance();
         for (int i = 0; i < 10; i++) {
             String pop = storehouse.pop();
-            log.debug("{}消费者消费{}", getName(), pop);
+            log.debug("{}消费{}", getName(), pop);
         }
     }
 }
